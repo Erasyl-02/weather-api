bdb3043f36fc3b52c40523bf3281c3af3478b39d
Erasyl-02 <103504281+Erasyl-02@users.noreply.github.com>
2026-01-26 21:49:58 +0500

    wip: taking api data

--- logic.py
+++ logic.py
@@ -1,6 +1,7 @@
 import requests
 from dotenv import load_dotenv
 import os
+import json
 
 url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/almaty?'
 
@@ -9,6 +10,7 @@ load_dotenv()
 API = os.environ.get('API_KEY')
 params = {
     'unitGroup': 'metric',
+    'include': 'days, current',
     'key': API,
     'contentType': 'json'
 }
@@ -16,5 +18,18 @@ params = {
 response = requests.get(url, params=params)
 
 
-response = response.json()
-print(response['address'])
+data = response.json()
+
+#print(json.dumps(data, indent = 2))
+
+print(f'''City: {data['resolvedAddress']}
+Datetime: {data['days'][0]['datetime']}
+Temperature: {data['days'][0]['temp']}
+
+Feels Like: {data['days'][0]['tempmax']}
+
+Minimal Temperature: {data['days'][0]['tempmin']}
+Maximal Temperature: {data['days'][0]['tempmax']}
+
+Sunrise time: {data['days'][0]['sunrise']}
+Sunset time: {data['days'][0]['sunset']}''')
